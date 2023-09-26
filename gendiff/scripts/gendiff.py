48bb090b325dec3a37ec166d49bc6b3e8a035e00
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2023-09-26 14:38:05 +0300

    Add output of reference information about utility
    At this stage, the gendiff utility, when executed on the command line,
    outputs only reference information about itself. This help is generated
    automatically using a special argparse module, which simplifies
    the creation of CLI utilities. This module is included in the standard
    Python library.

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import argparse
+
+def main():
+    # creating an ArgumentParser object for parsing the command line into 
+    # Python data types
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    
+    # adding positional arguments
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    # parsing arguments
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
