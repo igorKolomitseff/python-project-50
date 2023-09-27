1dd80f2ae73652d624beb2910e3b7f091c3b1e66
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2023-09-27 16:53:48 +0300

    Add get_argparse() function to parse.py

---
+++ gendiff/parse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def get_argparse() -> tuple:
+    """Allows the program to get the necessary arguments and track
+            the correctness of their use.
+
+    Returns:
+        A tuple of passed arguments.
+    """
+
+    # creating an ArgumentParser object for parsing the command line into
+    # Python data types
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+
+    # adding positional arguments
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    # adding optional argument
+    parser.add_argument('-f', '--format', help='set format of output')
+
+    # parsing arguments
+    args = parser.parse_args()
+
+    return args.first_file, args.second_file
