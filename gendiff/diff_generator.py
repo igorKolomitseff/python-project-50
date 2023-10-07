cc619c4fe0f43ca99b9df7cb00099b25624a0549
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2023-10-07 15:41:50 +0300

    Add formatter stylish to the diff_generator.py

--- gendiff/diff_generator.py
+++ gendiff/diff_generator.py
@@ -3,15 +3,19 @@ from gendiff.diff import make_diff
 from gendiff.formatter_stylish import format_diff
 
 
-def generate_diff(first_file_path: str,  second_file_path: str) -> str:
+def generate_diff(first_file_path: str,
+                  second_file_path: str,
+                  formatter='stylish') -> str:
     """Returns a change in the content in the second file relative to the first.
 
     Args:
         first_file_path: Path to the first file.
         second_file_path: Path to the second file.
+        formatter (optional): Nested dictionary formatter.
+            Defaults to 'stylish'.
 
     Returns:
-        A string containing the difference between files.
+        str:A string containing the difference between files.
     """
 
     first_file_dict = get_file_content(first_file_path)
