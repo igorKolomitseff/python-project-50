e5e300bf709e16bdcd89c72d8e80a863dfa2b596
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2023-10-13 14:16:40 +0300

    Update cli_parser.py

--- gendiff/cli_parser.py
+++ gendiff/cli_parser.py
@@ -17,15 +17,19 @@ def get_argparse() -> tuple:
 
     # adding positional arguments
     parser.add_argument('first_file',
-                        type=str)
+                        type=str,
+                        help='absolute or relative path to the first file')
     parser.add_argument('second_file',
-                        type=str)
+                        type=str,
+                        help='absolute or relative path to the second file')
 
     # adding optional argument
     parser.add_argument('-f',
                         '--format',
+                        type=str,
+                        choices=['stylish', 'plain', 'json'],
                         default='stylish',
-                        help='set format of output')
+                        help='set format of output (default: \'stylish\')')
 
     # parsing arguments
     args = parser.parse_args()
